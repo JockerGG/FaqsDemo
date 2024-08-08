c18b8e66165be5ff62e6fc0a614d52c24797d3c3
Eduardo Garcia Gonzalez <eduardo.garcia@elektra.com.mx>
2024-08-07 21:17:49 -0600

    Initial Commit

---
+++ FaqsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaqsDemo
+//
+//  Created by Eduardo Garcia Gonzalez on 07/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
